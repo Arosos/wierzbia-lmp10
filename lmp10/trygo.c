9e6855e2ca7097e4c140330525f6e581975470ae
Arosos <wierzba96@o2.pl>
2016-01-14 20:15:33 +0100

    Zmieniony Makefile i trygo.c

--- lmp10/trygo.c
+++ lmp10/trygo.c
@@ -5,33 +5,46 @@ void make_spl (points_t *pts, spline_t *spl)
 {
 	int i, j, m;
 	int n = pts->n;
-	double *x = pts->x;
-	double *y = pts->y;
-	m = (n-1)/2;
+	double *x = pts->x, *y = pts->y;
 	if (n % 2 == 0)
-		m = (n/2)-1;
+		m = n/2-1;
 	else
 		m = (n-1)/2-1;
-	double a0, a[m], b[m], sumay, sumaa, sumab, c, d = 2.0/n;
+	double p = x[0], k = x[n-1], a0, a[m], b[m], sumay, sumaa, sumab, c, d = 2.0/n, e;
 	sumay = sumaa = sumab = 0.0;
 	for (i = 0 ; i < n ; i++)
 		sumay += y[i];
 	a0 = sumay/n;
 	if (alloc_spl(spl, n) == 0)
 	{
-		spl->x = x;
-		spl->y = y;
-	}
-	for (i = 0 ; i < m ; i++)
-	{
-		spl->a[i] = spl->b[i] = 0.0;
-		for (j = 0 ; j < n ; j++)
+		spl->n = m;
+		for (i = 0 ; i < m ; i++)
 		{
-			c = 2.0*M_PI*(j+1)*(i+1)/n;
-			sumaa += y[j] * cos(c);
-			sumab += y[j] * sin(c);
+			for (j = 0 ; j < n ; j++)
+			{
+				c = 2.0*M_PI*j*i/n;
+				sumaa += y[j] * cos(c);
+				sumab += y[j] * sin(c);
+			}
+			a[i] = d*sumaa;
+			b[i] = d*sumab;
+			sumaa = sumab = 0.0;
 		}
-		spl->a[i] = d*sumaa;
-		spl->b[i] = d*sumab;
+		for(i = 0 ; i < n ; i++)
+		{
+			double xx = spl->x[i] = p + i*(k-p)/(m-1);
+			spl->f[i] = 0;
+			spl->f1[i] = 0;
+			spl->f2[i] = 0;
+			spl->f3[i] = 0;
+			for (j = 0 ; j < m ; j++)
+			{
+				e = 2.0*M_PI*j/n;
+				spl->f[i] += a[j]*cos(e*xx) + b[j]*sin(e*xx);
+				spl->f1[i] += (-1)*e*a[j]*sin(e*xx) + e*b[j]*cos(e*xx);
+				spl->f2[i] += (-1)*e*e*a[j]*cos(e*xx) + (-1)*e*e*b[j]*sin(e*xx);
+				spl->f3[i] += e*e*e*a[j]*sin(e*xx) + (-1)*e*e*e*b[j]*cos(e*xx);
+			}
+		}
 	}
 }
